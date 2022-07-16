967beb455bdcc293ebb21dd50b54090f5507326a
Ovidiu <ovidiu.catalin.stanescu@gmail.com>
2022-07-16 19:20:11 +0200

    list done

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -3,12 +3,55 @@ const router = require("express").Router()
 const Game = require('./../models/Games.model')
 
 
-router.get("/getgames", (req, res) => {
+router.get("/getAllGames", (req, res) => {
 
     Game
         .find()
         .then(response => res.json(response))
         .catch(err => res.status(500).json(err))
 })
+
+
+router.get("/getOneGame/:game_id", (req, res) => {
+
+    const { game_id } = req.params
+
+    Game
+        .findById(game_id)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+
+router.post("/addGame", (req, res) => {
+
+    Game
+        .create(req.body)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+
+router.post("/updateGame/:game_id", (req, res) => {
+
+    const { game_id } = req.params
+
+    Game
+        .findByIdAndUpdate(game_id, req.body)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+
+router.post("/deleteGame/:game_id", (req, res) => {
+
+    const { game_id } = req.params
+
+    Game
+        .findByIdAndDelete(game_id)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
 
 module.exports = router
