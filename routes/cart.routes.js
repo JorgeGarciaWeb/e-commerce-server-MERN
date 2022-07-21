465cdbb41e3f1e03a39c084a87f43bfb2c4dc8df
Ovidiu <ovidiu.catalin.stanescu@gmail.com>
2022-07-21 11:29:51 +0200

    rutas cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 
 const { isAuthenticated } = require('../middleware/jwt.middleware')
 const User = require('../models/User.model')
-const Cart = require('./../models/Cart.model')
 
 
 router.get('/getItems', isAuthenticated, (req, res) => {
@@ -13,6 +12,25 @@ router.get('/getItems', isAuthenticated, (req, res) => {
         .findById(user_id)
         .then(user => res.json({ items: user.items }))
         .catch(err => console.log(err))
+})
+
+
+router.post('/addItem', isAuthenticated, (req, res) => {
+
+    const { _id: user_id } = req.payload
+    const { game_id } = req.body
+
+    const item = {
+        product: game_id,
+        quantity: 1
+    }
+
+
+    User
+        .findByIdAndUpdate(user_id, { $push: { items: item } }, { new: true })
+        .then(response => res.json(response))
+        .catch(err => console.log(err))
+
 })
 
 module.exports = router
