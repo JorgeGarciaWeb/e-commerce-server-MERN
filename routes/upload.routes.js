ead3f5dc5768639f856a21a41d1bb15128022033
Ovidiu <ovidiu.catalin.stanescu@gmail.com>
2022-07-28 17:29:52 +0200

    all done

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -2,13 +2,15 @@ const router = require("express").Router()
 
 const uploader = require('./../config/cloudinary.config')
 
-router.post('/image', uploader.single('imageData'), (req, res) => {
+router.post('/image', uploader.array('imageData'), (req, res) => {
 
-    if (!req.file) {
+    if (!req.files) {
         res.status(500).json({ errorMessage: 'Error load the file' })
     }
 
-    res.json({ cloudinary_url: req.file.path })
+    const response = req.files.map(elm => elm.path)
+
+    res.json(response)
 })
 
 module.exports = router
